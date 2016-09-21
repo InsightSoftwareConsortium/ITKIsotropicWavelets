58fa9b6cf792765f489683d9ceee211cdada2052
Pablo Hernandez-Cerdan <pablo.hernandez.cerdan@outlook.com>
2016-09-21 20:28:10 +1200

    [WIP] Add Simoncelli Wavelet.
    Use abstraction of IsotropicWaveletFunction to avoid rewrite
    of SubBands methods.
    Just override EvaluateMagnitude for each new isotropic wavelet.

--- test/itkWaveletFrequencyFilterBankGeneratorTest.cxx
+++ test/itkWaveletFrequencyFilterBankGeneratorTest.cxx
@@ -24,6 +24,7 @@
 #include "itkWaveletFrequencyFilterBankGenerator.h"
 #include "itkHeldIsotropicWavelet.h"
 #include "itkVowIsotropicWavelet.h"
+#include "itkSimoncelliIsotropicWavelet.h"
 #include "itkForwardFFTImageFilter.h"
 #include "itkInverseFFTImageFilter.h"
 #include <itkComplexToRealImageFilter.h>
@@ -59,7 +60,8 @@ int runWaveletFrequencyFilterBankGeneratorTest( const std::string& inputImage,
 
   // Set the WaveletFunctionType and the WaveletFilterBank
   // typedef itk::HeldIsotropicWavelet<PixelType> WaveletFunctionType;
-  typedef itk::VowIsotropicWavelet<PixelType> WaveletFunctionType;
+  // typedef itk::VowIsotropicWavelet<PixelType> WaveletFunctionType;
+  typedef itk::SimoncelliIsotropicWavelet<PixelType> WaveletFunctionType;
   typedef itk::WaveletFrequencyFilterBankGenerator< ComplexImageType, WaveletFunctionType> WaveletFilterBankType;
   typename WaveletFilterBankType::Pointer forwardFilterBank = WaveletFilterBankType::New();
   unsigned int high_sub_bands = inputBands;
