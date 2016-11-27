e1d28c76827232683347528e04975e448a96e369
Pablo Hernandez-Cerdan <pablo.hernandez.cerdan@outlook.com>
2016-11-28 01:50:40 +1300

    [BUG] Change pixel type from double to float in test

--- test/itkRieszWaveletPhaseAnalysisTest.cxx
+++ test/itkRieszWaveletPhaseAnalysisTest.cxx
@@ -53,7 +53,7 @@ int runRieszWaveletPhaseAnalysisTest( const std::string& inputImage,
     const unsigned int& inputBands)
 {
   const unsigned int dimension = N;
-  typedef double                           PixelType;
+  typedef float                            PixelType;
   typedef itk::Image<PixelType, dimension> ImageType;
   typedef itk::ImageFileReader<ImageType>  ReaderType;
   typename ReaderType::Pointer reader = ReaderType::New();
@@ -104,7 +104,7 @@ int runRieszWaveletPhaseAnalysisTest( const std::string& inputImage,
   for (unsigned int nout = 0; nout < forwardWavelet->GetNumberOfOutputs(); ++nout)
     {
     std::cout << "************Nout: "<< nout << " / " <<noutputs << std::endl;
-    if(nout == 1200) // TODO check this. avoid phase analysis in last approximation (low_pass).
+    if(nout == 0) // TODO check this. avoid phase analysis in last approximation (low_pass).
       {
       modifiedWavelets.push_back(analysisWavelets[nout]);
     //TODO remove this visualize
