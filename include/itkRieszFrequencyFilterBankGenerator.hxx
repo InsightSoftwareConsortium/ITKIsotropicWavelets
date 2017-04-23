cf1dcb3c3ad4bf6756af646747ae9b3935cd1e54
Pablo Hernandez-Cerdan <pablo.hernandez.cerdan@outlook.com>
2017-04-23 18:36:52 +1200

    BUG: Fix python wrap of RieszFrequencyFilterFGenerator.
    The output is now complex, not real.

--- include/itkRieszFrequencyFilterBankGenerator.hxx
+++ include/itkRieszFrequencyFilterBankGenerator.hxx
@@ -83,7 +83,7 @@ void RieszFrequencyFilterBankGenerator< TOutputImage, TRieszFunction, TFrequency
       this->m_Evaluator->EvaluateAllComponents(frequencyIt.GetFrequency());
     for (unsigned int comp = 0; comp < this->GetNumberOfOutputs(); ++comp)
       {
-      outputItList[comp].Set( evaluatedArray[comp] );
+      outputItList[comp].Set( static_cast<typename OutputImageType::PixelType>(evaluatedArray[comp]) );
       ++outputItList[comp];
       }
     itkDebugMacro(<< "w_vector: " << frequencyIt.GetFrequency()
