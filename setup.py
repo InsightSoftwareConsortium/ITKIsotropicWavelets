757fca4d992d9e284c5945926fd30666e86328a1
Pablo Hernandez-Cerdan <pablo.hernandez.cerdan@outlook.com>
2017-08-06 18:42:01 +1200

    Python: Modify metadata in setup.

--- setup.py
+++ setup.py
@@ -19,16 +19,17 @@ setup(
     packages=['itk'],
     package_dir={'itk': 'itk'},
     download_url=r'https://github.com/phcerdan/ITKIsotropicWavelets',
-    description=r'',
+    description=r'Steerable Wavelet Pyramid with Isotropic Wavelets and Riesz Functions',
     long_description='ITKIsotropicWavelets provides a multiresolution analysis'
-    '(MRA) framework using isotropic and steerable wavelets in the frequency'
-    'domain. This framework provides the backbone for state of the art filters'
-    'for denoising, feature detection or phase analysis in N-dimensions.'
-    'It focus on reusability, and highly decoupled modules for easy extension'
-    'and implementation of new filters.'
-    'Please refer to:'
-    'P. Hernandez-Cerdan, “Isotropic and Steerable Wavelets in N Dimensions.'
-    'A multiresolution analysis framework”, Insight Journal, http://hdl.handle.net/10380/3558, 2017.',
+                     '(MRA) framework using isotropic and steerable wavelets in the frequency'
+                     'domain.\n'
+                     'This framework provides the backbone for state of the art filters'
+                     'for denoising, feature detection or phase analysis in N-dimensions.'
+                     'It focus on reusability, and highly decoupled modules for easy extension'
+                     'and implementation of new filters.\n'
+                     'Please refer to:'
+                     'P. Hernandez-Cerdan, “Isotropic and Steerable Wavelets in N Dimensions.\n'
+                     'A multiresolution analysis framework”, Insight Journal, http://hdl.handle.net/10380/3558, 2017.',
     classifiers=[
         "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python",
@@ -49,7 +50,7 @@ setup(
         "Operating System :: MacOS"
         ],
     license='Apache',
-    keywords='ITK InsightToolkit',
+    keywords='ITK InsightToolkit Wavelet Pyramid Steerable Riesz',
     url=r'https://github.com/phcerdan/ITKIsotropicWavelets',
     install_requires=[
         r'itk'
